81e658c55ec9ee09dddb546dcb831ab3daed8b07
Ian Edwards <49249205+ian-edwards@users.noreply.github.com>
2019-04-04 21:05:07 +0100

    Create quickgameshop.bat

---
+++ quickgameshop.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+title quickgameshop
+set /p search=Enter search text:
+set searchplus=%search: =+%
+set searchminus=%search: =-%
+set searchspace=!search: =%%20!
+start https://store.playstation.com/en-gb/search/%searchspace%
+start https://www.amazon.co.uk/s?k=%searchplus%
+start https://uk.webuy.com/search?stext=%searchspace%
+start https://www.base.com/fsearch.htm?search=%searchplus%
+start https://www.argos.co.uk/search/%searchminus%/
+start https://www.coolshop.co.uk/s/?q=%searchspace%
+start https://www.thegamecollection.net/#sqr:(q[%searchspace%])
+start https://www.shopto.net/Gsi/Search?newsearch=%searchplus%
